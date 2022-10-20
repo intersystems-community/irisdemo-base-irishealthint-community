05af2c0691663a2f8ad3f2ba3deb3435711ba2e0
Amir Samary <amirsamary@gmail.com>
2022-10-20 15:11:30 -0400

    Bumping IRIS version to 2022.1.0.209.0-zpm

--- build-arm.sh
+++ build-arm.sh
@@ -4,12 +4,9 @@ set -e
 
 DOCKER_REPO=intersystemsdc/irisdemo-base-irishealthint-community
 VERSION=`cat ./VERSION`
-echo ${DOCKER_REPO}:arm64-version-${VERSION}
-
 
 docker buildx use default
 
-
-docker buildx build --platform linux/arm64  -t ${DOCKER_REPO}:arm64-version-${VERSION} -f ./arm64.dockerfile .
+docker buildx build --platform linux/arm64 -t ${DOCKER_REPO}:arm64-version-${VERSION} -f ./arm64.dockerfile .
 
 docker tag ${DOCKER_REPO}:arm64-version-${VERSION} ${DOCKER_REPO}:arm64-latest
--- build-x86.sh
+++ build-x86.sh
@@ -5,6 +5,8 @@ set -e
 DOCKER_REPO=intersystemsdc/irisdemo-base-irishealthint-community
 VERSION=`cat ./VERSION`
 
-docker buildx build  --platform linux/amd64 -f ./amd64.dockerfile -t ${DOCKER_REPO}:amd64-version-${VERSION} .
+docker buildx use default
+
+docker buildx build --platform linux/amd64 -f ./amd64.dockerfile -t ${DOCKER_REPO}:amd64-version-${VERSION} .
 
 docker tag ${DOCKER_REPO}:amd64-version-${VERSION} ${DOCKER_REPO}:amd64-latest
